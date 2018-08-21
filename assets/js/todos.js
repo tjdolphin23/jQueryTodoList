a2e170e6bcdc79f1e8deca9d8c57faab20586150
Tom Dolphin <thedolphingroup1@gmail.com>
2018-08-21 09:11:14 -0700

    removing todo item from list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,10 @@ $("li").click(function() {
 	$(this).toggleClass("completed");
 });
 
+//X button delete todo item
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
