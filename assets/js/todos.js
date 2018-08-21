8fc336009b42bafde5a1c4ef0ac23232b956f403
Tom Dolphin <thedolphingroup1@gmail.com>
2018-08-21 10:12:55 -0700

    completed todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -4,7 +4,7 @@ $("ul").on("click", "li", function() {
 });
 
 //X button delete todo item
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
@@ -18,6 +18,10 @@ $("input[type=text]").keypress(function(event) {
 		var todoText = $(this).val();
 		$(this).val("");
 		//create new li and add to ul
-		$("ul").append("<li><span>X</span>" + todoText + "</li");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li");
 	}
 });
+
+$(".fa-plus").click(function( ){
+	$("input[type=text]").fadeToggle();
+})
